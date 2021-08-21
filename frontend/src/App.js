b9153f8cb661edf185f86165dc823e546d7d4e2f
Justin Brown <justinb@advancedreimbursement.us>
2021-08-20 20:53:31 -0700

    Init Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import { Header, Footer } from './components'
+
+function App() {
+  return (
+    <div>
+
+        <Header />
+    <main className="App">
+       eCommerce
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
